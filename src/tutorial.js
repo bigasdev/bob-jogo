73eea73f9091818ce3371bf9501084782740ad9e
gustavo domingues <deathdiec@gmail.com>
2023-05-02 11:23:57 -0300

    feat: fixed some css

--- src/tutorial.js
+++ src/tutorial.js
@@ -1,39 +1,45 @@
 import { changeState, states } from "./state.js";
 
-export default function startTutorialApp(){
-    document.getElementById('tutorial').style.display = "block";
+export default function startTutorialApp() {
+    document.getElementById("tutorial").style.display = "inline-block";
 }
-export function closeTutorial(){
-    document.getElementById('tutorial').style.display = 'none';
+export function closeTutorial() {
+    document.getElementById("tutorial").style.display = "none";
 }
 
 //Numero atual do tutorial
-var tutorialImgName = "./assets/tutorial/tutorial_"
+var tutorialImgName = "./assets/tutorial/tutorial_";
 var tutorialState = 0;
 var tutorialAmt = 3;
 
-export function startTutorial(){
-    document.getElementById('tutorial_prox').addEventListener('click',()=>{
+export function startTutorial() {
+    document.getElementById("tutorial_prox").addEventListener("click", () => {
         next();
-    })
-    document.getElementById('tutorial_anterior').addEventListener('click',()=>{
-        anterior();
-    })
-    document.getElementById('tutorial_pular').addEventListener('click',()=>{
+    });
+    document
+        .getElementById("tutorial_anterior")
+        .addEventListener("click", () => {
+            anterior();
+        });
+    document.getElementById("tutorial_pular").addEventListener("click", () => {
         pular();
-    })
+    });
 }
 
-function pular(){
+function pular() {
     closeTutorial();
     changeState(states.playing);
 }
 
-function next(){
-    if(tutorialState < tutorialAmt-1)tutorialState++;
-    document.getElementById('tutorial_img').src = `./assets/tutorial/tutorial_${tutorialState}.jpeg`
+function next() {
+    if (tutorialState < tutorialAmt - 1) tutorialState++;
+    document.getElementById(
+        "tutorial_img"
+    ).src = `./assets/tutorial/tutorial_${tutorialState}.jpeg`;
 }
-function anterior(){
-    if(tutorialState > 0)tutorialState--;
-    document.getElementById('tutorial_img').src = `./assets/tutorial/tutorial_${tutorialState}.jpeg`
+function anterior() {
+    if (tutorialState > 0) tutorialState--;
+    document.getElementById(
+        "tutorial_img"
+    ).src = `./assets/tutorial/tutorial_${tutorialState}.jpeg`;
 }
