90cea59c7e63b437c5e93192701a4fcefcfdb085
gustavo domingues <deathdiec@gmail.com>
2023-05-02 10:09:20 -0300

    feat: added webpack to bundle everything

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    entry: "./src/app.js", // specify the entry point of your application
+    output: {
+        filename: "app.js", // specify the name of the output file
+        path: __dirname + "/dist", // specify the output directory
+    },
+};
